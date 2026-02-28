6e4c02c24de581380520f060ddd0b32f1f4af2b6
Augusto Caesar <aoghast@gmail.com>
2026-02-28 16:16:53 -0300

    understanding how functions and arrow functions work in JS

---
+++ 4_functions.js
@@ -0,0 +1,19 @@
+// function greetings(name){
+//     // console.log("Howdy");
+//     return `Howdy ${name}`
+// }
+// console.log(greetings("Augusto"));
+
+// const greetings = (name) =>{
+//     return `Howdy ${name}`
+// }
+// console.log(greetings("Augusto"));
+
+const sum = (x, y) => x + y;
+const difference = (x, y) => x - y;
+const product = (x, y) => x * y;
+const dividend = (x, y) => x / y;
+console.log(sum(2,2));
+console.log(difference(2,3));
+console.log(product(2,2));
+console.log(dividend(4,2));
