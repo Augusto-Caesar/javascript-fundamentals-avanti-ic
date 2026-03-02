04d3bebb6bc25e833329756c5e25bd8ee4aebb30
Augusto Caesar <aoghast@gmail.com>
2026-03-02 16:45:01 -0300

    working with insertAdjacentHTML method

--- DOM-Project/JS/scripts.js
+++ DOM-Project/JS/scripts.js
@@ -12,9 +12,19 @@ const classText = document.getElementsByClassName("text");
 //in order to edit elements grouped by class, you need "for of"
 for(text of classText){
     text.style.color = "#00a";
+    text.style.fontSize = "15px";
 };
 //to manipulate by tagname, you'll also need a "for of"
 const titles = document.getElementsByTagName("h2");
 for(title of titles){
     title.style.color = "#0Aa";
+    title.style.fontSize = "25px";
 };
+
+const divFeature = document.querySelector("#divFeature");
+const newElement = document.createElement("p");
+newElement.textContent = "A brand new paragraph made wholly through JS!";
+divFeature.append(newElement);
+//below method works the same as above, but in a simpler way
+divFeature.insertAdjacentHTML("beforeend", `<p>Another paragraph made wholly in JS!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>`)//template strings allow to breakline
