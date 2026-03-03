822334f44c265918e75fa5070db4b8bd6b34aba3
Augusto Caesar <aoghast@gmail.com>
2026-03-03 15:04:37 -0300

    further exploring events through form actions and hoisting functions

--- Events-Project/JS/scripts.js
+++ Events-Project/JS/scripts.js
@@ -2,6 +2,11 @@ const divContent = document.querySelector("#divContent");
 const btnShow = document.querySelector("#btn-show");
 const btnHide = document.querySelector("#btn-hide");
 const btnHideShow = document.querySelector("#btn-hideShow");
+
+const inputForm = document.querySelector("#get");
+const btnGet = document.querySelector("#btn-get");
+const divResult = document.querySelector("#divResult");
+
 btnShow.addEventListener("click", () => {
     divContent.classList.add("show");
     divContent.classList.remove("hide");
@@ -9,7 +14,32 @@ btnShow.addEventListener("click", () => {
 btnHide.addEventListener("dblclick", () => {
     divContent.classList.add("hide");
     divContent.classList.remove("show");
-})
+});
 btnHideShow.addEventListener("click", () => {
     divContent.classList.toggle("hide");
-})
+});
+
+inputForm.addEventListener("keydown", (e)=>{
+    console.log(e.key);
+});
+
+inputForm.addEventListener("focus", (e)=>{
+    inputForm.style.backgroundColor = "#FF0";
+});
+
+inputForm.addEventListener("blur", (e)=>{
+    inputForm.style.backgroundColor = "";
+});
+
+btnGet.addEventListener("click", (e) => {
+    searchResult(); //function being hoisted
+});
+
+const searchResult = () => {
+    const searchValue = inputForm.value.trim();
+    if(searchValue){
+        divResult.textContent = `You searched for: ${searchValue}`;
+    }else{
+        divResult.textContent = ``;
+    };
+};
