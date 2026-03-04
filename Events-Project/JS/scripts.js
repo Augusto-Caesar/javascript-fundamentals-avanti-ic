298c9bfbbec77057e11f48ddbfbd6d90133763c5
Augusto Caesar <aoghast@gmail.com>
2026-03-04 15:11:29 -0300

    created scripts for consuming the github user api, implemented a userCard via JS scripting to be shown on the frontend with avatar image, userlogin and userbio

--- Events-Project/JS/scripts.js
+++ Events-Project/JS/scripts.js
@@ -1,3 +1,45 @@
+const createUserCard = (user) => {
+    const divApp = document.getElementById("divApp");
+    const userCard = document.createElement("div");
+    userCard.className = "card";
+
+    const avatar = document.createElement("img");
+    avatar.src = user.avatar_url;
+    avatar.alt = `${user.login}'s Avatar`;
+    const name = document.createElement("h2");
+    name.textContent = user.name;
+    const login = document.createElement("p");
+    login.textContent = `@${user.login}`;
+    const bio = document.createElement("p");
+    bio.textContent = `${user.bio}`;
+
+    userCard.appendChild(avatar);
+    userCard.appendChild(name);
+    userCard.appendChild(login);
+    userCard.appendChild(bio);
+    divApp.appendChild(userCard);
+};
+
+const fetchGithubUser = (username) =>{
+    fetch(`https://api.github.com/users/${username}`)
+        .then((response) => {
+            if(!response.ok){
+                throw new Error(`Error: ${response.status}`)
+            };
+        return response.json()})
+        .then((user) =>{
+            console.log("User", user)
+            createUserCard(user);
+        })
+        .catch((error) =>{
+            console.error(error)
+            const divApp = document.getElementById("divApp")
+            divApp.innerHTML = `<p style="color: red">Error: ${error.message}</p>`
+        })
+}
+
+fetchGithubUser("Augusto-Caesar");
+
 const divContent = document.querySelector("#divContent");
 const btnShow = document.querySelector("#btn-show");
 const btnHide = document.querySelector("#btn-hide");
